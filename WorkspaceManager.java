95a9f59a4c800391d3b5d672071cd4ee5b94f80a
sPatel28-OSU <patel.5851@buckeyemail.osu.edu>
2025-10-25 11:48:03 -0400

    Added test comment

--- WorkspaceManager.java
+++ WorkspaceManager.java
@@ -1,28 +1,11 @@
-import java.util.*;import java.util.*;
+import java.util.*;
+import java.io.*;
 
-import java.io.*;import java.io.*;
+public class WorkspaceManager {
+    private static final String WORKSPACE_DIR = "workspaces/";
+    // Added a comment to test git
 
-
-
-
-
-public class WorkspaceManager {public class WorkspaceManager {
-
-        
-
-    private static final String WORKSPACE_DIR = "workspaces/";    private static final String WORKSPACE_DIR = "workspaces/";
-
-
-
-    public WorkspaceManager() {    public WorkspaceManager{
-
-        new File(WORKSPACE_DIR).mkdirs();        new File(WORKSPACE_DIR).mkdirs();
-
-    }    }
-
-}
-
-
-
-
+    public WorkspaceManager() {
+        new File(WORKSPACE_DIR).mkdirs();
+    } 
 }
